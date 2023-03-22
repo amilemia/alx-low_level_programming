b05f57bc13e61686b69204c8a5b9c5bae873e6f9
Amilemia <aminemadah2@gmail.com>
2023-03-22 07:01:55 +0000

    indentation fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,36 +4,36 @@
  * print_times_table - Prints the times table of the input,
  *
  * @n: The value of the times table to be printed.
-*/
+ */
 void print_times_table(int n)
 {
-int num, mult, result;
+	int num, mult, result;
 	if (n <= 15 && n >= 0)
-{
-	for (num = 0; num <= n; num++)
 	{
-		_putchar('0');
-	for (mult = 1; mult <= n; mult++)
-	{
-		_putchar(',');
-		_putchar(' ');
-			result = num * mult;
-	if (result <= 99)
-		_putchar(' ');
-	if (result <= 9)
-		_putchar(' ');
-	if (result >= 100)
-{
-		_putchar((result / 100) + '0');
-		_putchar((result / 10) % 10 + '0');
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				result = num * mult;
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
-	else if (result <= 99 && result >= 10)
-{
-		_putchar((result / 10) + '0');
-	}
-		_putchar((result % 10) + '0');
-}
-		_putchar('\n');
-}
-}
 }
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
-* main - prints sum of even fibonacci
-* suit elements
-* Return: Always 0
-*/
+ * main - prints sum of even fibonacci
+ * suit elements
+ * Return: Always 0
+ */
 int main(void)
 {
-unsigned long fib1 = 0, fib2 = 1, fib3 = 0, sum = 0;
-while (fib3 <= 4000000)
-{
-fib3 = fib1 + fib2;
-fib1 = fib2;
-fib2 = fib3;
+	unsigned long fib1 = 0, fib2 = 1, fib3 = 0, sum = 0;
+	while (fib3 <= 4000000)
+	{
+		fib3 = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib3;
 
-if ((fib1 % 2) == 0)
-sum += fib1;
+		if ((fib1 % 2) == 0)
+			sum += fib1;
 
-}
-printf("%ld\n", sum);
-return (0);
+	}
+	printf("%ld\n", sum);
+	return (0);
 }
