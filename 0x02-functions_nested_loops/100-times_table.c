766a9fff9eff828292a58360129d6fc8ad06aaec
Amilemia <aminemadah2@gmail.com>
2023-03-22 02:33:36 +0000

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,34 +8,32 @@
 void print_times_table(int n)
 {
 int num, mult, result;
-    if (n <= 15 && n >= 0)
-    {
-        for (num = 0; num <= n; num++)
-        {
-        _putchar('0');
-
-        for (mult = 1; mult <= n; mult++)
+	if (n <= 15 && n >= 0)
 {
-        _putchar(',');
-        _putchar(' ');
-            result = num * mult;
-    if (result <= 99)
-        _putchar(' ');
-
-    if (result <= 9)
-        _putchar(' ');
-    if (result >= 100)
-    {
-        _putchar((result / 100) + '0');
-        _putchar((result / 10) % 10 + '0');
+	for (num = 0; num <= n; num++)
+	{
+		_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+			result = num * mult;
+	if (result <= 99)
+		_putchar(' ');
+	if (result <= 9)
+		_putchar(' ');
+	if (result >= 100)
+{
+		_putchar((result / 100) + '0');
+		_putchar((result / 10) % 10 + '0');
+	}
+	else if (result <= 99 && result >= 10)
+{
+		_putchar((result / 10) + '0');
+	}
+		_putchar((result % 10) + '0');
 }
-    else if (result <= 99 && result >= 10)
-    {
-        _putchar((result / 10) + '0');
+		_putchar('\n');
 }
-        _putchar((result % 10) + '0');
-}
-        _putchar('\n');
-        }
 }
 }
