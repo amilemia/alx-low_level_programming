c48515d7877f3b9867a17d9037f8e16a26ff7a99
Amilemia <aminemadah2@gmail.com>
2023-03-22 03:15:53 +0000

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,12 +3,13 @@
 /**
  * main - Prints first 50 Fibonacci numbers.
  *
- * Return: 0.
+ * Return: Always 0.
  */
 int main(void)
 {
 	int inc;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+	
 	for (inc = 0; inc < 50; inc++)
 	{
 	sum = fib1 + fib2;
