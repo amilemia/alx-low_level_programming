6e74e71bdf50e80bbe9cbca80f0a1abb3705aac3
Amilemia <aminemadah2@gmail.com>
2023-03-22 06:58:41 +0000

    indentation

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,20 @@
  */
 int main(void)
 {
-unsigned long fib1 = 0, fib2 = 1, sum;
-int inc;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	int inc;
 
 	for (inc = 0; inc < 50; inc++)
-{
-	sum = fib1 + fib2;
-printf("%lu", sum);
-fib1 = fib2;
-fib2 = sum;
-if (inc == 49)
-printf("\n");
-else
-printf(", ");
-}
-return (0);
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+		fib1 = fib2;
+		fib2 = sum;
+		if (inc == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
 }
 
