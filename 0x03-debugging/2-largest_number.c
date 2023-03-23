5bb2d785dc80a1161ba5c1c5783cc07cd973349a
Amilemia <aminemadah2@gmail.com>
2023-03-23 05:13:01 +0000

    Revert "0 > 972"
    This reverts commit 8c53db15efd5788429ca1eb156da5ca7167e3951.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,6 +18,11 @@ int largest_number(int a, int b, int c)
         largest = a;
     }
     else if (b >= a && b >= c)
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
     {
         largest = b;
     }
