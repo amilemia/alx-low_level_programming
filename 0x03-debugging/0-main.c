340da9a81cd58480f433c5bda2ceffb8b72cf233
Amilemia <aminemadah2@gmail.com>
2023-03-23 00:44:35 +0000

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (succ)
+ **/
+
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive_or_negative(i);
+	return(0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+void positive_or_negative(int i);
+
+#endif
