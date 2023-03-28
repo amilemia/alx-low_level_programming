45230a4c7b0f0b36b55f65f1d202cfd9e6a218e0
Amilemia <aminemadah2@gmail.com>
2023-03-28 02:14:53 +0000

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string
+ * @s: The string
+ */
+
+void rev_string(char*s)
+{
+	int i, len = 0;
+	char swap;
+
+	while (s[len])
+	{
+		len++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		swap = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = swap;
+	}
+}
