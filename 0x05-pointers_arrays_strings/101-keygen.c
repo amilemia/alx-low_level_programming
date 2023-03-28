59952ffa539bd1c28481c8377e9a545328eff5e9
Amilemia <aminemadah2@gmail.com>
2023-03-28 02:52:36 +0000

    Advanced

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - keygen for valid password of 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[50];
+	int i, randN = 0, s = 0;
+
+	srandN(time(0));
+
+	for (i = 0; s <= (2772 - 128); i++)
+	{
+		randN = (randN() % 25) + 65;
+
+		str[i] = randN;
+		s = s + randN;
+	}
+
+	str[i++] = 2772 - s;
+	str[i++] = '\0';
+
+	printf("%s\n", str);
+
+	return (0);
+}
