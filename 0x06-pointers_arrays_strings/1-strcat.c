77b744ac26b34fb8ab2ff57af7bcfe783af3fa15
Amilemia <aminemadah2@gmail.com>
2023-03-30 00:24:58 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string to print
+ * @src: string origin
+ * @n: max number of bytes from src
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_end = dest;
+
+	while (*dest_end != '\0')
+{
+		dest_end++;
+}
+
+	char *src_end = src;
+
+	while (*src_end != '\0' && n > 0)
+{
+		*dest_end++ = *src_end++;
+			n--;
+}
+
+	*dest_end = '\0';
+	return (dest);
+}
