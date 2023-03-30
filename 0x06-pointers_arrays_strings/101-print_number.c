62c26097b03038ac7ec397d7e19c271428d2f49b
Amilemia <aminemadah2@gmail.com>
2023-03-30 19:36:41 +0000

    Task 9, advanced

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,17 +7,22 @@
 
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int i;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
+		_putchar('-');
+		i = -n;
+	}
+	else
+	{
+		i = n;
 	}
 
 	if (i / 10)
 	{
 		print_number(i / 10);
 	}
+
 	_putchar(i % 10 + '0');
 }
