9613ca4d279e2bd930ff844fcce1917354dc3f73
Amilemia <aminemadah2@gmail.com>
2023-03-30 01:07:32 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse the content of an array of integers
+ * @a: Array to reverse
+ * @n: Number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
+
