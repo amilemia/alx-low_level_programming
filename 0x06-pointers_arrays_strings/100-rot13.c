6d0d862e47fa5295ce3a39b58c81b4298589ac39
Amilemia <aminemadah2@gmail.com>
2023-03-30 19:13:16 +0000

    Task 8, advanced

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13 encryption
+ * @str: The string to encrypt
+ *
+ * Return: Encrypted string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (alpha[j] != '\0')
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
