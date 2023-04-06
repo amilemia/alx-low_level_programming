29492e90fdd75575bfed683255c5a9e2492e053a
Amilemia <aminemadah2@gmail.com>
2023-04-06 00:54:07 +0000

    Task 7, Advanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -20,6 +20,28 @@ int str_len(char *s)
 }
 
 /**
+ * checkPal - Check if a string is a palindrome
+ * @s: The string to check
+ * @start: Starting index of string
+ * @end: Ending index of string
+ * 
+ * Return: 1 if palindrome, 0 otherwise
+*/
+
+int checkPal(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return (checkPal(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome
  * @s: The string to check
  *
@@ -36,12 +58,8 @@ int is_palindrome(char *s)
 	{
 		return (1);
 	}
-	else if (*s != *(s + len - 1))
-	{
-		return (0);
-	}
 	else
 	{
-		return (is_palindrome(s + 1) && 1);
+		return (checkPal(s, 0, len - 1));
 	}
 }
