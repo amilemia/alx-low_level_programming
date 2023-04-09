4c5802139d259e4db4a68e6e286f0a7e4608858b
Amilemia <aminemadah2@gmail.com>
2023-04-09 03:25:03 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argv[0])
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
