f50927cba13a419f5179a8dbb7977ec49c82f9a9
Amilemia <aminemadah2@gmail.com>
2023-04-14 00:38:06 +0000

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,12 +17,12 @@ int *array_range(int min, int max)
 	int *array, i = 0, j = min;
 
 	if (min > max)
-		return (0);
+		return (NULL);
 
 	array = malloc((max - min + 1) * sizeof(int));
 
 	if (!array)
-		return (0);
+		return (NULL);
 
 	while (i <= max - min)
 		array[i++] = j++;
