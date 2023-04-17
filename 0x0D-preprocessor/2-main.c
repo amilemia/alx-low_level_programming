6d201e19b9567859a9d109477bbcd6b2fe33144e
Amilemia <aminemadah2@gmail.com>
2023-04-17 20:23:22 +0000

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,17 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the file it was compiled from,
+ *              followed by a new line. It uses the __FILE__ macro to get the
+ *              name of the current source file being compiled, and prints it
+ *              using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+ 
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
     printf("Compiled from file: %s\n", __FILE__);
     return (0);
