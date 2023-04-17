a1521ecdbcf62be19193163d9b7c04b52de18153
Amilemia <aminemadah2@gmail.com>
2023-04-17 22:12:54 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog with a name, age, and owner
+ *
+ * @name: a pointer to the dog's name (a C-string)
+ * @age: the dog's age in years (a float)
+ * @owner: a pointer to the dog's owner's name (a C-string)
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
