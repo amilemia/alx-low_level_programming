51b19c7229f8e2de1b979c63258d957446a49dbb
Amilemia <aminemadah2@gmail.com>
2023-04-19 21:53:02 +0000

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
-	if (array && action)
+	if (array == NULL || action == NULL)
+		return;
 
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
