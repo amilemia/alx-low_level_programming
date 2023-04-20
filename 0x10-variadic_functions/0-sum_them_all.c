4387004136cd93adf0ca595590796ad5c388f2ed
Amilemia <aminemadah2@gmail.com>
2023-04-20 22:58:54 +0000

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,12 +1,12 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - Computes the sum of a variable number of integers.
+ * sum_them_all - Compute the sum of a variable number of integers
  *
- * @n: The number of integers to sum.
- * @...: The integers to sum.
+ * @n: The number of integers to sum
+ * @...: The integers to sum
  *
- * Return: The sum of the integers, or 0 if @n is 0.
+ * Return: The sum of the integers, or 0 if @n is 0
  */
 
 int sum_them_all(const unsigned int n, ...)
