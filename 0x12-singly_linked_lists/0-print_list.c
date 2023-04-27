b94fddb1920e37979a8276afce9079715be6d06a
Amilemia <aminemadah2@gmail.com>
2023-04-28 00:01:23 +0100

    Debug check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t count_nodes = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+    while (h != NULL)
+    {
+        count_nodes++;
 
-		h = h->next;
-		node_count++;
-	}
-	return (node_count);
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+        h = h->next;
+    }
+	
+	return (count_nodes);
 }
