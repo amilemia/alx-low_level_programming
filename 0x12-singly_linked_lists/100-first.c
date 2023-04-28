2806d26a44d03aaac4d3807fd1f3dedba561950e
Amilemia <aminemadah2@gmail.com>
2023-04-28 01:45:51 +0100

    Task 5, advanced debug

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * print_before_main - prints a message before main function is executed
+ * pre_main - prints a message before main function is executed
  */
 
-void print_before_main(void)
+void __attribute__((constructor)) pre_main()
 {
 	printf("You're beat! and yet, you must allow,\n"
 		   "I bore my house upon my back!\n");
