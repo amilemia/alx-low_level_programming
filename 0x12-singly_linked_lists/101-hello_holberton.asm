ae42f97cff2eb3a7515233d6e69c0991272d51b4
Amilemia <aminemadah2@gmail.com>
2023-04-29 01:17:08 +0100

    Task 6, advanced

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+section .data
+	hello db "Hello, Holberton!", 0
+	fmt db "%s\n", 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+	
+	; call printf with hello and fmt as arguments
+	mov rdi, fmt
+	mov rsi, hello
+	xor rax, rax
+	call printf
+	
+	; return 0
+	mov eax, 0
+	
+	; clean up stack and exit
+	leave
+	ret
+	
