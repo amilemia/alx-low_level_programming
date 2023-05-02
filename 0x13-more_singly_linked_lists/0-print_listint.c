cf28307ffa0b7b73d9ac3c31e24b12e966252005
Amilemia <aminemadah2@gmail.com>
2023-05-02 20:23:48 +0100

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 
 	return (count);
