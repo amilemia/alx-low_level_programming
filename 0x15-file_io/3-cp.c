25ab16eb993a930f1b691e90488f87481137714b
Amilemia <aminemadah2@gmail.com>
2023-05-09 01:54:01 +0100

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,11 +7,16 @@
  * @message: The error message to print
  * @filename: The name of the file to use in the error message
  * @exit_code: The exit code to use when exiting the program
-*/
+ */
 
 void print_error(char *message, char *filename, int exit_code)
 {
-	dprintf(STDERR_FILENO, message, filename);
+	exit_code = (exit_code == 0) ? -1 : exit_code;
+	if (filename == NULL)
+		dprintf(STDERR_FILENO, message, "");
+	else
+		dprintf(STDERR_FILENO, message, filename);
+
 	exit(exit_code);
 }
 
@@ -19,7 +24,7 @@ void print_error(char *message, char *filename, int exit_code)
  * copy_file - Copy the content of one file to another
  * @file_from: The name of the file to copy from
  * @file_to: The name of the file to copy to
-*/
+ */
 
 void copy_file(char *file_from, char *file_to)
 {
@@ -32,26 +37,31 @@ void copy_file(char *file_from, char *file_to)
 
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (fd_to == -1)
+	{
+		close(fd_from);
 		print_error("Error: Can't write to %s\n", file_to, 99);
+	}
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
 			print_error("Error: Can't write to %s\n", file_to, 99);
-
-		if (bytes_written != bytes_read)
-			print_error("Error: Write to %s failed\n", file_to, 99);
+		}
 	}
 
 	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
 		print_error("Error: Can't read from file %s\n", file_from, 98);
+	}
 
-	if (close(fd_from) == -1)
-		print_error("Error: Can't close fd %d\n", fd_from, 100);
-
-	if (close(fd_to) == -1)
-		print_error("Error: Can't close fd %d\n", fd_to, 100);
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+		print_error("Error: Can't close fd\n", NULL, 100);
 }
 
 /**
@@ -60,18 +70,20 @@ void copy_file(char *file_from, char *file_to)
  * @argv: Array of pointers to the strings which are the arguments
  *
  * Return: 0 on success, 97, 98, 99 or 100 on failure
-*/
+ */
 
 int main(int argc, char **argv)
 {
+	char *file_from, *file_to;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	char *file_from = argv[1];
-	char *file_to = argv[2];
+	file_from = argv[1];
+	file_to = argv[2];
 
 	copy_file(file_from, file_to);
 
