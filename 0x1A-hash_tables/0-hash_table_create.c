8e8505bb0525a856bef43ecf03b9bf7d7944225a
Amilemia <aminemadah2@gmail.com>
2023-07-11 04:23:24 +0100

    Edit: Task 0, 7

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,26 +3,32 @@
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
- * Return: a pointer to the newly created hash table
-**/
+ * Return: pointer to the newly created hash table, or NULL if fails
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
-	if (!size)
+	if (size == 0)
 		return (NULL);
 
-	ht = calloc(1, sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!ht->array)
 	{
 		free(ht);
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	ht->size = size;
 
 	return (ht);
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
+	hash_node_t *temp = NULL, *next = NULL;
+	unsigned long int i = 0;
 
-	for (i = 0; i < ht->size; i++)
+	if (!ht)
+		return;
+
+	for (; i < ht->size; ++i)
 	{
-		if (ht->array[i] != NULL)
+		temp = ht->array[i];
+		while (temp)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
 		}
 	}
-	free(head->array);
-	free(head);
+
+	free(ht->array);
+	free(ht);
 }
